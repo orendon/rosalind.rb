ddfa98b8a22402d84d636ab908b0aec59b1f0c42
Oscar Rendon <oarendon@gmail.com>
2013-01-12 18:32:35 -0500

    implements nucleotide class

---
+++ ruby/lib/dna.rb
@@ -0,0 +1,27 @@
+class Nucleotide
+
+  attr_reader :dna
+
+  NUCLEOBASES = { adenine: 'A', cytosine: 'C', guanine: 'G', thymine: 'T' }
+
+  def initialize(dna)
+    @dna = dna
+    @count = {}
+  end
+
+  def nucleobases_count
+    perform_count
+    "#{adenine} #{cytosine} #{guanine} #{thymine}"
+  end
+
+  def method_missing(name, *args, &block)
+    return @count[name] if NUCLEOBASES.keys.include?(name)
+    super
+  end
+
+  private
+
+  def perform_count
+    NUCLEOBASES.each { |k, v| @count[k] = @dna.count(v) }
+  end
+end
